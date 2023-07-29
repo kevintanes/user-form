1a7edb081eac155a0a23ee6e0a1fb8e485f4fd33
kevintanes <kevin.tanes@gmail.com>
2023-07-30 01:50:53 +0700

    env file

--- firebase/index.ts
+++ firebase/index.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNG_BQB-YVu6smDgnMpLhWILtvQ5D2nFc",
-  authDomain: "userlist-form.firebaseapp.com",
-  projectId: "userlist-form",
-  storageBucket: "userlist-form.appspot.com",
-  messagingSenderId: "166678630254",
-  appId: "1:166678630254:web:8e7f7261dcc030c7e3bb7f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
